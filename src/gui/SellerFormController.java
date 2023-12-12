4d08c40db34c466461bfa75186a291c23feb9a5a
Cesar <cesar.socin@gmail.com>
2023-12-12 16:23:04 -0200

    TextField & DatePicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -18,6 +21,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -39,7 +43,25 @@ public class SellerFormController implements Initializable {
 	private TextField txName;
 	
 	@FXML
+	private TextField txEmail;
+	
+	@FXML
+	private DatePicker dpBirthDate;
+	
+	@FXML
+	private TextField txBaseSalary;
+	
+	@FXML
 	private Label labelErrorName;
+	
+	@FXML
+	private Label labelErrorEmail;
+	
+	@FXML
+	private Label labelErrorBirthDate;
+	
+	@FXML
+	private Label labelErrorBaseSalary;
 	
 	@FXML
 	private Button btSave;
@@ -117,7 +139,10 @@ public class SellerFormController implements Initializable {
 
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txId);
-		Constraints.setTextFieldMaxLength(txName, 30);
+		Constraints.setTextFieldMaxLength(txName, 70);
+		Constraints.setTextFieldDouble(txBaseSalary);
+		Constraints.setTextFieldMaxLength(txEmail, 60);
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
 	}
 	
 	public void updateFormData() {
@@ -127,6 +152,13 @@ public class SellerFormController implements Initializable {
 		
 		txId.setText(String.valueOf(entity.getId()));
 		txName.setText(entity.getName());
+		txEmail.setText(entity.getEmail());
+		Locale.setDefault(Locale.US);
+		txBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+		if (entity.getBirthDate() != null) {
+			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));	
+		}
+		
 	}
 	
 	private void setErrorMessages(Map<String, String> errors) {
